89c0d623a763d6891b9c8e3cc105a98d0ccfa663
vsrikar <vsrikar@LIN80027789.corp.capgemini.com>
2022-10-15 13:25:25 +0530

    Recruiter,feedback,Bookmarkedfreelancer entities and dtos are created by srikar.

---
+++ src/main/java/com/jobportal/dto/RecruiterDTO.java
@@ -0,0 +1,101 @@
+package com.jobportal.dto;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.jobportal.entity.BookmarkedFreelancer;
+import com.jobportal.entity.Feedback;
+import com.jobportal.entity.Job;
+
+public class RecruiterDTO {
+	
+	private long id;
+	private String firstName;
+	private String lastName;
+	private List<Job> postedJobs;
+	private List<Feedback> Feedbacks;
+	private List<BookmarkedFreelancer> freelancers;
+	
+	
+	
+	public RecruiterDTO() {
+		super();
+	}
+	
+	
+	public RecruiterDTO(long id, String firstName, String lastName, List<Job> postedJobs, List<Feedback> feedbacks,
+			List<BookmarkedFreelancer> freelancers) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.postedJobs = postedJobs;
+		Feedbacks = feedbacks;
+		this.freelancers = freelancers;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public List<Job> getPostedJobs() {
+		return postedJobs;
+	}
+	public void setPostedJobs(List<Job> postedJobs) {
+		this.postedJobs = postedJobs;
+	}
+	public List<Feedback> getFeedbacks() {
+		return Feedbacks;
+	}
+	public void setFeedbacks(List<Feedback> feedbacks) {
+		Feedbacks = feedbacks;
+	}
+	public List<BookmarkedFreelancer> getFreelancers() {
+		return freelancers;
+	}
+	public void setFreelancers(List<BookmarkedFreelancer> freelancers) {
+		this.freelancers = freelancers;
+	}
+	@Override
+	public String toString() {
+		return "RecruiterDTO [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", postedJobs="
+				+ postedJobs + ", Feedbacks=" + Feedbacks + ", freelancers=" + freelancers + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(Feedbacks, firstName, freelancers, id, lastName, postedJobs);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecruiterDTO other = (RecruiterDTO) obj;
+		return Objects.equals(Feedbacks, other.Feedbacks) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(freelancers, other.freelancers) && id == other.id
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(postedJobs, other.postedJobs);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/jobportal/entity/BookmarkedFreelancer.java
@@ -0,0 +1,61 @@
+package com.jobportal.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class BookmarkedFreelancer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "skill_id", unique = true)
+	private Skill skill;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "freelancer_id", unique = true)
+	private Freelancer freelancer;
+	//Is there any relation to be add here?
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "recruiter_id", unique = true)
+	private Recruiter bookmarkedBy;
+	
+	public BookmarkedFreelancer(long id, Recruiter bookmarkedBy, Skill skill, Freelancer freelancer) {
+		super();
+		this.id = id;
+		this.skill = skill;
+		this.freelancer = freelancer;
+		this.bookmarkedBy = bookmarkedBy;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Skill getSkill() {
+		return skill;
+	}
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+	public Freelancer getFreelancer() {
+		return freelancer;
+	}
+	public void setFreelancer(Freelancer freelancer) {
+		this.freelancer = freelancer;
+	}
+	public Recruiter getBookmarkedBy() {
+		return bookmarkedBy;
+	}
+	public void setBookmarkedBy(Recruiter bookmarkedBy) {
+		this.bookmarkedBy = bookmarkedBy;
+	}
+	
+
+}
+
