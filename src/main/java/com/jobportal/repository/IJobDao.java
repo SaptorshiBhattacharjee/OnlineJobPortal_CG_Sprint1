b36a64a76be8aa6ab43deb8d24a587e57d4ea943
Swetha <justitsit@gmail.com>
2022-10-15 16:16:50 +0530

    added job entity ,dto,dao

---
+++ src/main/java/com/jobportal/repository/IJobDao.java
@@ -0,0 +1,11 @@
+
+package com.jobportal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jobportal.entity.Job;
+
+public interface IJobDao extends CrudRepository<Job, Integer>{
+ 
+
+}
