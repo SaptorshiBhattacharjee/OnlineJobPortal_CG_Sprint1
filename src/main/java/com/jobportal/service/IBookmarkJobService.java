e3ee866914158b78cdc7d4bc0085c3512a0841b8
SAPTBHAT <SAPTBHAT@inlen8520018163.corp.capgemini.com>
2022-10-18 02:00:54 +0530

    API commit

--- src/main/java/com/jobportal/service/IBookmarkJobService.java
+++ src/main/java/com/jobportal/service/IBookmarkJobService.java
@@ -2,6 +2,7 @@ package com.jobportal.service;
 
 import java.util.List;
 
+import com.jobportal.dto.BookmarkedJobDTO;
 import com.jobportal.entity.BookmarkedJob;
 import com.jobportal.entity.Freelancer;
 import com.jobportal.entity.Job;
@@ -13,9 +14,5 @@ public interface IBookmarkJobService
 	public BookmarkedJob bookmarkJob(Job job,Freelancer freelancer)throws JobPortalException;
 	public void removeBookmark(Job job,Freelancer freelancer)throws JobPortalException;
 	public List<BookmarkedJob> findBookmarkedJobsBySkill(Skill skill,Freelancer freelancer)throws JobPortalException;
-<<<<<<< HEAD
-	public BookmarkedJob findById(Integer id)throws JobPortalException;
-=======
-	public BookmarkedJob findById(Long id)throws JobPortalException;
->>>>>>> branch 'main' of https://github.com/SaptorshiBhattacharjee/OnlineJobPortal_CG_Sprint1.git
+	public BookmarkedJobDTO findById(Integer id)throws JobPortalException;
 }
--- src/main/java/com/jobportal/service/IBookmarkJobServiceImpl.java
+++ src/main/java/com/jobportal/service/IBookmarkJobServiceImpl.java
@@ -1,11 +1,17 @@
 package com.jobportal.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import com.jobportal.dto.BookmarkedJobDTO;
@@ -28,35 +34,58 @@ public class IBookmarkJobServiceImpl implements IBookmarkJobService
 	@Override
 	public BookmarkedJob bookmarkJob(Job job, Freelancer freelancer) throws JobPortalException 
 	{
+		BookmarkedJob bookmarkedJob = new BookmarkedJob();
+		bookmarkedJob.setJob(job);
+		bookmarkedJob.setFreelancer(freelancer);
+		bookmarkedJob.setSkill(job.getSkill());
+		bookmarkedJob.setId(job.getId());
 		
-		return null;
+		iBookMarkedJobDao.save(bookmarkedJob);
+		return bookmarkedJob;
 	}
 
+	@PersistenceContext
+	private EntityManager entityManager;
 	@Override
 	public void removeBookmark(Job job, Freelancer freelancer) throws JobPortalException 
 	{
+		Optional<BookmarkedJob> op1 = iBookMarkedJobDao.findById(job.getId());
+		BookmarkedJob bookmarkedJob = op1.orElseThrow(() -> new JobPortalException("Service.BOOKMARKED_JOB_NOT_FOUND"));
 		
+		Query query = (Query) entityManager.createQuery("DELETE FROM Job j,Freelancer f where j.id = f.id"); 
+		//extract job.id and free.id the write the query to delete
+		//iBookMarkedJobDao.deleteById(job.getId());
+		//iBookMarkedJobDao.del
 	}
 
 	@Override
 	public List<BookmarkedJob> findBookmarkedJobsBySkill(Skill skill, Freelancer freelancer) throws JobPortalException 
 	{
+		//freelancer
+		List<Freelancer> list = new ArrayList<>();
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(skill.getName().equals(freelancer.getBookmarkedJobs().toString()))
+			{
+				return(freelancer.getBookmarkedJobs());
+			}
+		}
 		return null;
 	}
 
 	@Override
-	public BookmarkedJob findById(Integer id) throws JobPortalException 
+	public BookmarkedJobDTO findById(Integer id) throws JobPortalException 
 	{
 		Optional<BookmarkedJob> op1 = iBookMarkedJobDao.findById(id);
 		BookmarkedJob bookmarkedJob = op1.orElseThrow(() -> new JobPortalException("Service.BOOKMARKED_JOB_NOT_FOUND"));
 		
 		//adding data to the DTO by id
 		BookmarkedJobDTO bookmarkedJobDTO = new BookmarkedJobDTO();
-		bookmarkedJob.setId(bookmarkedJobDTO.getId());
-		bookmarkedJob.setSkill(bookmarkedJobDTO.getSkill());
-		bookmarkedJob.setJob(bookmarkedJobDTO.getJob());
-		bookmarkedJob.setFreelancer(bookmarkedJobDTO.getFreelancer());
+		bookmarkedJobDTO.setId(bookmarkedJob.getId());
+		bookmarkedJobDTO.setSkill(bookmarkedJob.getSkill());
+		bookmarkedJobDTO.setJob(bookmarkedJob.getJob());
+		bookmarkedJobDTO.setFreelancer(bookmarkedJob.getFreelancer());
 		
-		return bookmarkedJob;
+		return bookmarkedJobDTO;
 	}
 }
