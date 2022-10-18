b427452c059ec1ee296e08e756c90471d4b2dc4e
Karthik Sreenivasan <krtsrnvsn@gmail.com>
2022-10-18 09:01:19 +0530

    commit by karthik

--- src/test/java/com/example/demo/OnlineJobPortalSprint1ApplicationTests.java
+++ src/test/java/com/example/demo/OnlineJobPortalSprint1ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jobportal.dto.AdminDTO;
+import com.jobportal.exception.InvalidAdminException;
+import com.jobportal.repository.IAdminDao;
+import com.jobportal.service.IAdminService;
+import com.jobportal.service.IAdminServiceImpl;
 
 @SpringBootTest
 class OnlineJobPortalSprint1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
