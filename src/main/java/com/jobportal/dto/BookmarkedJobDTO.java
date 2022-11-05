5a8813076ac2c847c88ef49f38bc4f329babd4d5
SAPTBHAT <SAPTBHAT@pl-ktw-atr-f4-cscs-std-01-vl1.net.capgemini.com>
2022-11-06 01:20:53 +0530

    commit by saptorshi 06112022

--- src/main/java/com/jobportal/dto/BookmarkedJobDTO.java
+++ src/main/java/com/jobportal/dto/BookmarkedJobDTO.java
@@ -1,17 +1,42 @@
 package com.jobportal.dto;
 
+import java.util.Optional;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.jobportal.entity.BookmarkedJob;
 import com.jobportal.entity.Freelancer;
 import com.jobportal.entity.Job;
 import com.jobportal.entity.Skill;
+import com.jobportal.entity.SkillExperience;
+import com.jobportal.repository.IFreelancerDao;
+import com.jobportal.repository.IJobDao;
+import com.jobportal.repository.ISkillDao;
+import com.jobportal.repository.ISkillExperienceDao;
 
 public class BookmarkedJobDTO 
 {
+	@Autowired
+	ISkillDao iSkillDao;
+	
+	@Autowired
+	IJobDao iJobDao;
+	
+	@Autowired
+	IFreelancerDao iFreelancerDao;
 	
 	private Integer bookmarkedJobDtoId;
-	private Skill skill;
-	private Job job;
-	private Freelancer freelancer;
+//	private Skill skill;
+//	private Job job;
+//	private Freelancer freelancer;
+	@NotNull(message="{bookmarkedJob.skill.absent}")
+	private Integer skillId;
+	@NotNull(message="{bookmarkedJob.job.absent}")
+	private Integer jobId;
+	@NotNull(message="{bookmarkedJob.freelancer.absent}")
+	private Integer freelancerId;
 	
 	// defining the getter and setter methods
 	public Integer getId() {
@@ -20,38 +45,50 @@ public class BookmarkedJobDTO
 	public void setId(Integer bookmarkedJobDtoId) {
 		this.bookmarkedJobDtoId = bookmarkedJobDtoId;
 	}
-	public Skill getSkill() {
-		return skill;
+	public Integer getSkillId() {
+		return skillId;
 	}
-	public void setSkill(Skill skill) {
-		this.skill = skill;
+	public void setSkillId(Integer skillId) {
+		this.skillId = skillId;
 	}
-	public Job getJob() {
-		return job;
+	public Integer getJobId() {
+		return jobId;
 	}
-	public void setJob(Job job) {
-		this.job = job;
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
 	}
-	public Freelancer getFreelancer() {
-		return freelancer;
+	public Integer getFreelancerId() {
+		return freelancerId;
 	}
-	public void setFreelancer(Freelancer freelancer) {
-		this.freelancer = freelancer;
+	public void setFreelancerId(Integer freelancerId) {
+		this.freelancerId = freelancerId;
 	}
 	
 	//defining toString method
-	@Override
+	/*@Override
 	public String toString() {
 		return "BookmarkedJobDTO [bookmarkedJobDtoId=" + bookmarkedJobDtoId + ", skill=" + skill + ", job=" + job + ", freelancer=" + freelancer + "]";
-	}
+	}*/
 	
 	public BookmarkedJob toBookmarkedJobEntity()
 	{
 		BookmarkedJob bookmarkedJob = new BookmarkedJob();
+		
 		bookmarkedJob.setId(this.getId());
-		bookmarkedJob.setSkill(this.getSkill());
-		bookmarkedJob.setJob(this.getJob());
-		bookmarkedJob.setFreelancer(this.getFreelancer());
+//		Optional<Job> optional = iJobDao.findById(this.jobId);
+//		Job job = optional.orElse(null);
+//		jobApplication.setJob(job);
+		Optional<Skill> optionalSkill = iSkillDao.findById(this.skillId);
+		Skill skill = optionalSkill.orElse(null);
+		bookmarkedJob.setSkill(skill);
+		
+		Optional<Job> optionalJob = iJobDao.findById(this.jobId);
+		Job job = optionalJob.orElse(null);
+		bookmarkedJob.setJob(job);
+		
+		Optional<Freelancer> optionalFreelancer = iFreelancerDao.findById(this.freelancerId);
+		Freelancer freelancer = optionalFreelancer.orElse(null);
+		bookmarkedJob.setFreelancer(freelancer);
 		
 		return(bookmarkedJob);
 	}
